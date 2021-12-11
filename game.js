8ffffe9fd6e9b6696ca8c3d17000d6a81648c909
Kyle Evans <krevan88@pm.me>
2021-12-10 19:40:52 -0700

    MVP complete

--- game.js
+++ game.js
@@ -24,7 +24,7 @@ let autoUp = {
     }
 };
 
-let souls = 0;
+let souls = 5000;
 let clickMult = 1.25;
 let autoMult = 1.75;
 
@@ -36,49 +36,73 @@ function harvest() {
     drawSouls()
 };
 
-function buyScythe() {
-    if (souls >= clickUp.scythe.price) {
-        clickUp.scythe.quantity++
-        souls -= clickUp.scythe.price
-        clickUp.scythe.price *= clickMult
+function buyClick(key) {
+    if (souls >= clickUp[key].price) {
+        clickUp[key].quantity++
+        souls -= clickUp[key].price
+        clickUp[key].price *= clickMult
         drawSouls()
     }
-    document.getElementById('sharpPrice').innerText = clickUp.scythe.price.toFixed(0)
-    document.getElementById('sharpTotal').innerText = clickUp.scythe.quantity
+    document.getElementById(`${key}Price`).innerText = clickUp[key].price.toFixed(0)
+    document.getElementById(`${key}Total`).innerText = clickUp[key].quantity
+    document.getElementById(`${key}Mult`).innerText = clickUp[key].multiplier * clickUp[key].quantity
 }
 
-function buyWeight() {
-    if (souls >= clickUp.weight.price) {
-        clickUp.weight.quantity++
-        souls -= clickUp.weight.price
-        clickUp.weight.price *= clickMult
+function buyAuto(key) {
+    if (souls >= autoUp[key].price) {
+        autoUp[key].quantity++
+        souls -= autoUp[key].price
+        autoUp[key].price *= autoMult
         drawSouls()
     }
-    document.getElementById('weightPrice').innerText = clickUp.weight.price.toFixed(0)
-    document.getElementById('weightTotal').innerText = clickUp.weight.quantity
+    document.getElementById(`${key}Price`).innerText = autoUp[key].price.toFixed(0)
+    document.getElementById(`${key}Total`).innerText = autoUp[key].quantity
+    document.getElementById(`${key}Mult`).innerText = autoUp[key].multiplier * autoUp[key].quantity
 }
 
-function buyAbsorb() {
-    if (souls >= autoUp.absorb.price) {
-        autoUp.absorb.quantity++
-        souls -= autoUp.absorb.price
-        autoUp.absorb.price *= autoMult
-        drawSouls()
-    }
-    document.getElementById('absorbPrice').innerText = autoUp.absorb.price.toFixed(0)
-    document.getElementById('absorbTotal').innerText = autoUp.absorb.quantity
-}
+// function buyScythe() {
+//     if (souls >= clickUp.scythe.price) {
+//         clickUp.scythe.quantity++
+//         souls -= clickUp.scythe.price
+//         clickUp.scythe.price *= clickMult
+//         drawSouls()
+//     }
+//     document.getElementById('sharpPrice').innerText = clickUp.scythe.price.toFixed(0)
+//     document.getElementById('sharpTotal').innerText = clickUp.scythe.quantity
+// }
 
-function buyMinion() {
-    if (souls >= autoUp.minion.price) {
-        autoUp.minion.quantity++
-        souls -= autoUp.minion.price
-        autoUp.minion.price *= autoMult
-        drawSouls()
-    }
-    document.getElementById('minionPrice').innerText = autoUp.minion.price.toFixed(0)
-    document.getElementById('minionTotal').innerText = autoUp.minion.quantity
-}
+// function buyWeight() {
+//     if (souls >= clickUp.weight.price) {
+//         clickUp.weight.quantity++
+//         souls -= clickUp.weight.price
+//         clickUp.weight.price *= clickMult
+//         drawSouls()
+//     }
+//     document.getElementById('weightPrice').innerText = clickUp.weight.price.toFixed(0)
+//     document.getElementById('weightTotal').innerText = clickUp.weight.quantity
+// }
+
+// function buyAbsorb() {
+//     if (souls >= autoUp.absorb.price) {
+//         autoUp.absorb.quantity++
+//         souls -= autoUp.absorb.price
+//         autoUp.absorb.price *= autoMult
+//         drawSouls()
+//     }
+//     document.getElementById('absorbPrice').innerText = autoUp.absorb.price.toFixed(0)
+//     document.getElementById('absorbTotal').innerText = autoUp.absorb.quantity
+// }
+
+// function buyMinion() {
+//     if (souls >= autoUp.minion.price) {
+//         autoUp.minion.quantity++
+//         souls -= autoUp.minion.price
+//         autoUp.minion.price *= autoMult
+//         drawSouls()
+//     }
+//     document.getElementById('minionPrice').innerText = autoUp.minion.price.toFixed(0)
+//     document.getElementById('minionTotal').innerText = autoUp.minion.quantity
+// }
 
 function drawSouls() {
     document.getElementById('souls').innerText = souls.toFixed(0)
@@ -95,5 +119,5 @@ function collectAutoUpgrades() {
 
 
 
-setInterval(collectAutoUpgrades, 2000);
+setInterval(collectAutoUpgrades, 1000);
 drawSouls()
